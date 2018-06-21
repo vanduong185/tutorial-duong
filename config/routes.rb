018317b2abf2e6b93a7e60a4582ea95971cb867d
duongnv <vanduong18598@gmail.com>
2018-06-22 00:39:38 +0700

    finish chap-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  #root 'users#index'
+  root 'application#hello'
 end
